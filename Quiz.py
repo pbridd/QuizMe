816d90987882057b7a19418aed8565ed55166b39
pbridd <pbridd@users.noreply.github.com>
2019-03-21 21:23:14 -0700

    added order and constructed question functionality to quiz

--- Quiz.py
+++ Quiz.py
@@ -1,6 +1,6 @@
 import Question
 import Answer
-from string import ascii_lowercase
+from string import ascii_uppercase
 
 class Quiz():
     def __init_(self):
@@ -8,19 +8,40 @@ class Quiz():
 
     def __init__(self, quiz_name):
         self.quiz_name = quiz_name
-        self.question_list = []
+        self.question_map = {}
 
-    def add_question(self, question_text, answer_list, preserve_answer_order):
-        if len(question_text) == 0 or len(answer_list) == 0:
+    def add_question(self, question_text, answer_list, preserve_answer_order, question_number):
+        if self.should_reject_question(question_text, answer_list, question_number):
             return False
+
         new_question = Question.Question(preserve_answer_order)
         new_question.question_text = question_text
+        new_question.question_number = question_number
 
         answer_index = 1
         for answer_text in answer_list:
-            curr_encoding = ascii_lowercase[answer_index - 1]
+            curr_encoding = ascii_uppercase[answer_index - 1]
             new_question.add_answer(answer_text, str(curr_encoding))
             answer_index += 1
 
-        self.question_list.append(new_question)
+        self.question_map[question_number] = new_question
+
         return True
+
+    def add_constructed_question(self, constructed_question):
+        if self.should_reject_question(constructed_question.question_text, constructed_question.answer_map,
+                                       constructed_question.question_number):
+            return False
+
+        self.question_map[constructed_question.question_number] = constructed_question
+
+        return True
+
+    def question_number_exists(self, question_number):
+        return question_number in self.question_map
+
+    def should_reject_question(self, question_text, answer_list, question_number):
+        if len(question_text) == 0 or len(answer_list) == 0 or self.question_number_exists(question_number):
+            return True
+
+        return False
--- test_collateral/test_Quiz.py
+++ test_collateral/test_Quiz.py
@@ -1,5 +1,6 @@
 import unittest
 from Quiz import *
+import Question
 
 
 class MyTestCase(unittest.TestCase):
@@ -8,9 +9,26 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(test_quiz.quiz_name, "my quiz")
 
         test_answer_list = ["Charizard", "Pikachu", "Blastoise", "Venusaur"]
-        test_quiz.add_question("Who is your favorite pokemon?", test_answer_list, False)
-        added_question = test_quiz.question_list[0]
+        test_quiz.add_question("Who is your favorite pokemon?", test_answer_list, False, 2)
+        added_question = test_quiz.question_map[2]
         self.assertEqual(added_question.question_text, "Who is your favorite pokemon?")
+
+        constructed_question = Question.Question(True)
+        constructed_question.question_text = "What's up?"
+        constructed_question.add_answer("Not much", "A")
+        constructed_question.question_number = 1
+        test_quiz.add_constructed_question(constructed_question)
+        self.assertEqual(len(test_quiz.question_map), 2)
+        added_question = test_quiz.question_map[1]
+        self.assertEqual(added_question.question_text, "What's up?")
+
+        bad_question = Question.Question(True)
+        bad_question.question_text = "I'm a terrible question!"
+        self.assertFalse(test_quiz.add_constructed_question(bad_question))
+        bad_question.add_answer("what kind of answer is this?", "E")
+        bad_question.question_number = 1
+        self.assertFalse(test_quiz.add_constructed_question(bad_question))
+
 
 
 if __name__ == '__main__':
